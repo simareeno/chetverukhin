89745161df503957437768eb195d553d1e1b4b95
Simon <sima.chetverukhin@inn.ru>
2017-01-29 22:04:12 +0300

    building gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,24 @@ var gulp = require('gulp'),
 		path = require('path'),
 		pug = require('gulp-pug'),
 		less = require('gulp-less'),
-		autoprefixer = require('autoprefixer'),
+		autoPrefixer = require('autoprefixer'),
 		minify = require('cssnano'),
-		sourcemaps = require('gulp-sourcemaps'),
+		sourceMaps = require('gulp-sourcemaps'),
 		concat = require('gulp-concat'),
 		plumber = require('gulp-plumber'),
+		cache = require('gulp-cache'),
 		notify = require('gulp-notify'),
 		uglify = require('gulp-uglify'),
-		postcss = require('gulp-postcss'),
+		postCss = require('gulp-postcss'),
+		imageMin = require('imagemin'),
+		browserSync = require('browser-sync').create(),
+		rollup = require('gulp-rollup'),
+		babel = require('rollup-plugin-babel'),
+		useref = require('gulp-useref'),
+		runSequence = require('run-sequence'),
+		webpack = require('webpack-stream'),
+		del = require('del'),
 		join = path.join;
-
 
 
 var DEST = 'out',
@@ -21,11 +29,10 @@ var DEST = 'out',
 		SCRIPTS = join(SRC, 'scripts');
 
 
-
 gulp.task('less', function() {
 
 	var processors = [
-		autoprefixer({browsers: ['last 2 versions']}),
+		autoPrefixer({browsers: ['last 2 versions']}),
 		minify()
 	];
 
@@ -38,14 +45,22 @@ gulp.task('less', function() {
 				}
 			})
 		}))
-		.pipe(sourcemaps.init())
+		.pipe(sourceMaps.init())
 		.pipe(less())
-		.pipe(postcss(processors))
+		.pipe(postCss(processors))
 		.pipe(concat('styles.css'))
-		.pipe(sourcemaps.write())
+		.pipe(sourceMaps.write())
 		.pipe(gulp.dest(join(DEST, 'styles')));
 });
 
+
+gulp.task('images', function(){
+	return gulp.src(join(SRC, 'images/**/*.+(png|jpg|jpeg|gif|svg)')
+	.pipe(cache(imagemin({
+			interlaced: true
+		})))
+	.pipe(gulp.dest(join(DEST, 'images'))
+});
 
 
 gulp.task('pug-index', function() {
@@ -64,3 +79,76 @@ gulp.task('pug-index', function() {
 		}))
 		.pipe(gulp.dest(DEST));
 });
+
+
+gulp.task('js', function() {
+
+	return gulp.src(join(SCRIPTS, 'app.js'))
+		.pipe(plumber({
+			errorHandler: notify.onError(function(err) {
+				return {
+					title: "JS",
+					message: err.message
+				}
+			})
+		}))
+		.pipe(webpack({
+			output: {
+				filename: 'bundle.js',
+			},
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest(join(DEST, 'scripts')));
+});
+
+
+gulp.task('clean:dist', function() {
+	return del.sync(DEST);
+})
+
+
+gulp.task('pug-index:sync', ['pug-index'], function(done) {
+	browserSync.reload();
+	done();
+});
+
+
+gulp.task('less:sync', ['less'], function(done) {
+	browserSync.reload();
+	done();
+});
+
+
+gulp.task('js:sync', ['js'], function(done) {
+	browserSync.reload();
+	done();
+});
+
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: './out'
+		},
+	})
+	gulp.watch(join(TEMPLATES, '**/*.pug'), ['pug-index:sync']);
+	gulp.watch(join(STYLES, '**/*.less'), ['less:sync']);
+	gulp.watch(join(SCRIPTS, '**/*.js'), ['js:sync']);
+	gulp.watch(join(SRC, 'img/*'), ['images']);
+})
+
+
+gulp.task('watch', ['browserSync'], function() {
+	gulp.watch(join(TEMPLATES, '**/*.pug'), ['pug-index']);
+	gulp.watch(join(STYLES, '**/*.less'), ['less']);
+	gulp.watch(join(SCRIPTS, '**/*.js'), ['js']);
+	gulp.watch(join(SRC, 'images/*'), ['images']);
+})
+
+
+gulp.task('build', function (callback) {
+	runSequence('clean:dist',
+		['pug-index', 'less', 'js', 'images'],
+		callback
+	)
+})
---
+++ src/scripts/app.js
@@ -0,0 +1,10 @@
+var $ = require('jquery/dist/jquery.min');
+var tween = require('gsap/src/minified/TweenMax.min');
+var packery = require('packery');
+var page = require('page');
+// var router = require('page.js');
+
+$('body').hide();
+
+// page.base('/basic');
+page()
